87cb30ac5430e209cab16e7516ab048a84a4d5ab
Amir Rajan <ar@amirrajan.net>
2016-08-10 07:50:46 -0500

    increase android api version (19 was too low)

--- config/android.rb
+++ config/android.rb
@@ -3,5 +3,5 @@
 Motion::Project::App.setup do |app|
   # Use `rake android:config' to see complete project settings.
   app.name = 'ColorGolfMobile'
-  app.api_version = '19'
+  app.api_version = '20'
 end
