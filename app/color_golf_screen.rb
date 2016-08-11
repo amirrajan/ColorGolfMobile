bddda65c68f9683d6f5bfc6b2ebc2d4ab16088ec
Amir Rajan <ar@amirrajan.net>
2016-08-11 18:59:37 -0500

    general layout in place for UI

--- app/color_golf_screen.rb
+++ app/color_golf_screen.rb
@@ -12,28 +12,49 @@ class ColorGolfScreen < UI::Screen
 
   def reload
     view.children.each { |x| view.delete_child(x) }
+    view.flex = 1
+    view.background_color = :white
+    view.margin = 5
 
-    render_root UI::View do |view|
-      view.flex = 1
-      view.margin = 25
-      view.background_color = :white
+    render :hole, UI::Label do |hole|
+      hole.margin = [25, 10, 5, 10]
+      hole.text = "Hole 1 of 9"
+      hole.text_alignment = :center
+      hole.font = { name: 'Existence-Light', size: 16, extension: :otf }
+    end
 
-      render :hole, UI::Label do |hole|
-        hole.margin = [10, 10, 5, 10]
-        hole.text = "Hole 1 of 9"
-        hole.text_alignment = :center
-        hole.font = { name: 'Existence-Light', size: 16, extension: :otf }
+    render! :header, UI::View do |header|
+      header.margin = 0
+      header.background_color = :white
+      render :square, UI::View do |square|
+        square.width = 80
+        square.height = 80
+        square.margin = 10
+        square.align_self = :center
+        square.background_color = :pink
       end
+    end
 
-      render :header, UI::View do |header|
-        header.height = 100
-        header.margin = 0
-        render :square, UI::View do |square|
-          square.width = 80
-          square.height = 80
-          square.margin = 10
-          square.align_self = :center
-          square.background_color = :red
+    header_width = get_view(:header).layout[2]
+
+    render :grid, UI::View do |grid|
+      grid.width = header_width
+      grid.margin = 10
+      grid.background_color = :white
+      grid.flex_direction = :row
+      grid.flex_wrap = :wrap
+      grid.justify_content = :center
+      grid.align_self = :center
+      21.times do |i|
+        render "button#{i}", UI::Button do |button|
+          button.width = (header_width - 10 - 6 - 5) / 3
+          button.height = 30
+          button.margin = 1
+          button.padding = 1
+          button.title = "100%"
+          button.font = { name: 'Existence-Light', size: 16, extension: :otf }
+          button.background_color = :white
+          button.color = :black
         end
       end
     end
--- app/view_generation.rb
+++ app/view_generation.rb
@@ -1,15 +1,25 @@
 module ViewGeneration
-  def render_root klass, &block
-    @current_parent = view
-    render :root, klass, &block
-  end
-
   def render id, klass
-    previous_parent = @current_parent
+    previous_parent = (@current_parent || view)
     v = klass.new
     @current_parent = v
     yield v
+    set_view id, v
     @current_parent = previous_parent
     @current_parent.add_child(v)
+  end
+
+  def render! id, klass, &block
+    render id, klass, &block
+    view.update_layout
+  end
+
+  def get_view id
+    @views[id]
+  end
+
+  def set_view id, view
+    @views ||= {}
+    @views[id] = view
   end
 end
