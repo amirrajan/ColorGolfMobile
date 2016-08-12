abdbd481c6db606b33ba7b31d7c17a4ac5312d72
Amir Rajan <ar@amirrajan.net>
2016-08-12 09:37:14 -0500

    more cleanup

--- app/color_golf_screen.rb
+++ app/color_golf_screen.rb
@@ -16,17 +16,18 @@ class ColorGolfScreen < UI::Screen
     render_view
   end
 
+  def font
+    { name: 'Existence-Light', size: 16, extension: :otf }
+  end
+
   def render_view
     view.children.each { |x| view.delete_child(x) }
     view.flex = 1
-    view.background_color = :white
     view.margin = 5
 
     render_hole
     render_target_color_square
     render_rgb_grid
-
-    view.update_layout
   end
 
   def render_hole
@@ -34,14 +35,13 @@ class ColorGolfScreen < UI::Screen
       hole.margin = [25, 10, 5, 10]
       hole.text = "Hole 1 of 9"
       hole.text_alignment = :center
-      hole.font = { name: 'Existence-Light', size: 16, extension: :otf }
+      hole.font = font
     end
   end
 
   def render_target_color_square
     render! :header, UI::View do |header|
       header.margin = 0
-      header.background_color = :white
       render :square, UI::View do |square|
         square.width = 80
         square.height = 80
@@ -55,10 +55,9 @@ class ColorGolfScreen < UI::Screen
   def render_rgb_grid
     header_width = width_for(:header)
 
-    render :grid, UI::View do |grid|
+    render! :grid, UI::View do |grid|
       grid.width = header_width
       grid.margin = 10
-      grid.background_color = :white
       grid.flex_direction = :row
       grid.flex_wrap = :wrap
       grid.justify_content = :center
@@ -70,11 +69,13 @@ class ColorGolfScreen < UI::Screen
           button.margin = 1
           button.padding = 1
           button.title = "100%"
+          #commenting out this block removes the exception
+          #=begin
           button.on :tap do
             puts "booya"
           end
-          button.font = { name: 'Existence-Light', size: 16, extension: :otf }
-          button.background_color = :white
+          #=end
+          button.font = font
           button.color = :black
         end
       end
