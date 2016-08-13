986af7f05195d70a0f207693f315d97298a5b02e
Amir Rajan <ar@amirrajan.net>
2016-08-12 21:59:52 -0500

    color changing working, need to get next hole working

--- app/color_golf_screen.rb
+++ app/color_golf_screen.rb
@@ -1,4 +1,6 @@
 class ColorGolfScreen < UI::Screen
+  attr_accessor :game
+
   include ViewGeneration
 
   def on_show
@@ -6,11 +8,11 @@ class ColorGolfScreen < UI::Screen
   end
 
   def on_load
+    $self = self
     @game = Game.new(
       colors_options_r: ["00", "33", "66", "99", "CC", "FF"],
       colors_options_g: ["00", "33", "66", "99", "CC", "FF"],
       colors_options_b: ["00", "33", "66", "99", "CC", "FF"])
-    $self = self
     render_view
     update
   end
@@ -22,12 +24,44 @@ class ColorGolfScreen < UI::Screen
   def update
     get_view(:hole).text = "Hole #{@game.hole} of 9"
     get_view(:target_color).background_color = @game.target_color
+
     if @game.player_color
       get_view(:player_color).background_color = @game.player_color
+      get_view(:player_color).text = " "
     else
       get_view(:player_color).background_color = :white
       get_view(:player_color).text = "?"
     end
+
+    cell_ids.each do |k, v|
+      get_view(k).background_color = :white
+      if(v[:value] == @game.send(v[:prop]))
+        get_view(k).background_color = "f5f5f5"
+      end
+    end
+  end
+
+  def cell_ids
+    @cell_ids ||= {
+      r_ff: { method: :swing_for_r, prop: :player_color_r, value: "ff", text: "100%" },
+      g_ff: { method: :swing_for_g, prop: :player_color_g, value: "ff", text: "100%" },
+      b_ff: { method: :swing_for_b, prop: :player_color_b, value: "ff", text: "100%" },
+      r_cc: { method: :swing_for_r, prop: :player_color_r, value: "cc", text: "80%"  },
+      g_cc: { method: :swing_for_g, prop: :player_color_g, value: "cc", text: "80%"  },
+      b_cc: { method: :swing_for_b, prop: :player_color_b, value: "cc", text: "80%"  },
+      r_99: { method: :swing_for_r, prop: :player_color_r, value: "99", text: "60%"  },
+      g_99: { method: :swing_for_g, prop: :player_color_g, value: "99", text: "60%"  },
+      b_99: { method: :swing_for_b, prop: :player_color_b, value: "99", text: "60%"  },
+      r_66: { method: :swing_for_r, prop: :player_color_r, value: "66", text: "40%"  },
+      g_66: { method: :swing_for_g, prop: :player_color_g, value: "66", text: "40%"  },
+      b_66: { method: :swing_for_b, prop: :player_color_b, value: "66", text: "40%"  },
+      r_33: { method: :swing_for_r, prop: :player_color_r, value: "33", text: "20%"  },
+      g_33: { method: :swing_for_g, prop: :player_color_g, value: "33", text: "20%"  },
+      b_33: { method: :swing_for_b, prop: :player_color_b, value: "33", text: "20%"  },
+      r_00: { method: :swing_for_r, prop: :player_color_r, value: "00", text: "0%"   },
+      g_00: { method: :swing_for_g, prop: :player_color_g, value: "00", text: "0%"   },
+      b_00: { method: :swing_for_b, prop: :player_color_b, value: "00", text: "0%"   }
+    }
   end
 
   def render_view
@@ -88,10 +122,12 @@ class ColorGolfScreen < UI::Screen
   end
 
   def render_rgb_grid
-    @header_width = width_for(:target_color_wraper)
+    header_width = width_for(:target_color_wraper)
+
+    cell_width = (header_width - 30).fdiv(3)
 
     render! :grid, UI::View do |grid|
-      grid.width = @header_width
+      grid.width = header_width
       grid.margin = 10
       grid.flex_direction = :row
       grid.flex_wrap = :wrap
@@ -99,7 +135,7 @@ class ColorGolfScreen < UI::Screen
       grid.align_self = :center
 
       render :r_header, UI::Label do |label|
-        label.width = (@header_width - 30).fdiv(3)
+        label.width = cell_width
         label.text = "R"
         label.align_self = :center
         label.text_alignment = :center
@@ -107,7 +143,7 @@ class ColorGolfScreen < UI::Screen
       end
 
       render :g_header, UI::Label do |label|
-        label.width = (@header_width - 30).fdiv(3)
+        label.width = cell_width
         label.text = "G"
         label.align_self = :center
         label.text_alignment = :center
@@ -115,20 +151,20 @@ class ColorGolfScreen < UI::Screen
       end
 
       render :b_header, UI::Label do |label|
-        label.width = (@header_width - 30).fdiv(3)
+        label.width = cell_width
         label.text = "B"
         label.align_self = :center
         label.text_alignment = :center
         label.font = font
       end
 
-      21.times do |i|
-        render_rgb_button i, "100%", (@header_width - 30).fdiv(3)
+      cell_ids.each do |k, v|
+        render_rgb_button k, v[:method], v[:value], v[:text], cell_width
       end
     end
   end
 
-  def render_rgb_button id, text, width
+  def render_rgb_button id, target_swing, value, text, width
     render id, UI::Button do |button|
       button.width = width
       button.height = 30
@@ -136,7 +172,8 @@ class ColorGolfScreen < UI::Screen
       button.padding = 1
       button.title = text
       button.on :tap do
-        button.background_color = "f5f5f5"
+        @game.send(target_swing, value)
+        update
       end
       button.font = font
       button.color = :black
