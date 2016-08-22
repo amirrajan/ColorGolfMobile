15a5dfb0ebb03ec5e3bc040630af20546832a0e7
Amir Rajan <ar@amirrajan.net>
2016-08-22 11:17:11 -0500

    hodor

--- config/android.rb
+++ config/android.rb
@@ -10,4 +10,5 @@ Motion::Project::App.setup do |app|
   # Use `rake android:config' to see complete project settings.
   app.name = 'ColorGolfMobile'
   app.api_version = '21'
+  app.archs << 'x86'
 end
