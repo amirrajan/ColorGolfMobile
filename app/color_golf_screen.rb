aa1dd87f0e9eeb6c55e711c98bbf5b536a62ab97
Amir Rajan <ar@amirraja.net>
2017-04-27 22:57:59 -0500

    fancy dsl

--- app/color_golf_screen.rb
+++ app/color_golf_screen.rb
@@ -1,7 +1,7 @@
 class ColorGolfScreen < UI::Screen
   attr_accessor :game
 
-  include ViewGeneration
+  # include ViewGeneration
 
   def on_show
     navigation.hide_bar
@@ -19,280 +19,119 @@ class ColorGolfScreen < UI::Screen
     new_game
     load_game
     set_random_stat_text
-    render_view
-    update
+    render markup, css
+    view.update_layout
   end
 
-  def update_hole
-    get_view(:hole).text = "Hole #{game.hole} of 9"
+  def square_with_border
+    [:view, { margin: 5 },
+     [:view, { background_color: :silver, width: 72, height: 72, align_self: :center },
+      [:view, { width: 70, height: 70, margin: 1, align_self: :center }]]]
   end
 
-  def update_colors
-    get_view(:target_color).background_color = game.target_color
-    get_view(:player_color).background_color = game.player_color || :white
+  def button_row value
+    [:view, { flex_direction: :row, margin: 3 },
+     3.times.map { [:button, { title: value }] }]
+  end
 
-    if game.player_color
-      get_view(:player_color).text = ' '
-    else
-      get_view(:player_color).text = '?'
+  def markup
+    [:view, { flex: 1, padding: 40 },
+     [[:label, { text: 'Hole 1 of 9' }],
+      [:label, { text: 'Par' }],
+      square_with_border,
+      square_with_border,
+      [:view, { flex_direction: :row, margin: 3 },
+       [[:label, { text: 'Red', flex: 1 }],
+        [:label, { text: 'Green', flex: 1 }],
+        [:label, { text: 'Blue', flex: 1 }]]],
+      button_row('0xff'),
+      button_row('0xbf'),
+      button_row('0x80'),
+      button_row('0x3f'),
+      button_row('0x00')]]
+  end
+
+  def css
+    {
+      label: { color: :black, text_alignment: :center },
+      button: { color: :black, flex: 1 }
+    }
+  end
+
+  def control_map
+    {
+      view: UI::View,
+      label: UI::Label,
+      button: UI:: Button
+    }
+  end
+
+  def special_keys
+    [:id, :tap]
+  end
+
+  def new_view view_symbol, attributes, styles
+    unless control_map.keys.include? view_symbol
+      puts "#{view_symbol} not supported"
+      return nil
     end
+
+    attributes = (styles[view_symbol] || {}).merge(attributes)
+
+    new_view = control_map[view_symbol].new
+
+    attributes.each do |k, v|
+      new_view.send("#{k}=", v) unless special_keys.include? k
+    end
+
+    attributes[:tap] && new_view.on(:tap) { send attributes[:tap] }
+
+    @views[attributes[:id]] = new_view if attributes[:id]
+
+    new_view
   end
 
-  def update_saturation_selection
-    cell_ids.each do |k, v|
-      if v[:value] == (game.send(v[:prop]) || '')
-        get_view(k).background_color = white_smoke
+  def control_definition? o
+    return false unless o
+    return false unless [2, 3].include? o.length
+    return false unless control_map.keys.include? o.first
+    true
+  end
+
+  def add_to_parent parent, definition, styles
+    v = new_view definition[0], definition[1], styles
+    content = definition[2]
+
+    if content
+      if control_definition?(content)
+        add_to_parent v, content, styles
+      elsif content.is_a? Array
+        content.each { |d| add_to_parent v, d, styles }
       else
-        get_view(k).background_color = :white
+        puts "#{content} is not supported"
       end
     end
+
+    parent.add_child v if v
   end
 
-  def update_next_hole_option
-    if game.correct?
-      get_view(:next_hole_button).hidden = false
-      get_view(:next_hole_button).height = 30
-    else
-      get_view(:next_hole_button).hidden = true
-    end
-
-    if game.over?
-      get_view(:next_hole_button).hidden = true
-      get_view(:new_game_button).hidden = false
-      get_view(:new_game_button).height = 30
-      get_view(:new_game_button).title = "Final Score: #{game.score}, Go Again"
-    else
-      get_view(:new_game_button).hidden = true
-    end
-  end
-
-  def update_score
-    get_view(:score).text = "Score: #{@game.score_string}"
-  end
-
-  def update_stats
-    get_view(:stat_text).text = @random_stat_text
-  end
-
-  def update
-    update_hole
-    update_colors
-    update_saturation_selection
-    update_next_hole_option
-    update_score
-    update_stats
-    view.update_layout
-    save_game
+  def render definition, styles
+    @views ||= {}
+    add_to_parent view, definition, styles
   end
 
   def set_random_stat_text
     @random_stat_text = Game.stats(Store['history'] || [], @game.hole)
   end
 
-  def cause_jni
-    true
-  end
-
   def available_percentages
     @available_percentages ||= [
       %w(ff 0xff),
+      %w(bf 0xbf),
       %w(80 0x80),
+      %w(3f 0x3f),
       %w(00 0x00)
     ]
-  end
-
-  def cell_ids
-    return @cell_ids if @cell_ids
-
-    @cell_ids = {}
-
-    available_percentages.each do |tuple|
-      @cell_ids["r_#{tuple[0]}".to_sym] = {
-        method: :swing_for_r,
-        prop: :player_color_r,
-        value: tuple[0],
-        text: tuple[1]
-      }
-
-      @cell_ids["g_#{tuple[0]}".to_sym] = {
-        method: :swing_for_g,
-        prop: :player_color_g,
-        value: tuple[0],
-        text: tuple[1]
-      }
-
-      @cell_ids["b_#{tuple[0]}".to_sym] = {
-        method: :swing_for_b,
-        prop: :player_color_b,
-        value: tuple[0],
-        text: tuple[1]
-      }
-    end
-
-    @cell_ids
-  end
-
-  def render_view
-    render_hole
-    render_final_score
-    render_target_color_square
-    render_player_color_square
-    render_rgb_grid
-    render_next_hole_option
-    render_stat_text
-    view.update_layout
-  end
-
-  def render_hole
-    render :hole, UI::Label do |hole|
-      hole.text = "Hole #{game.hole} of 9"
-      hole.margin = [20, 10, 5, 10]
-      hole.text_alignment = :center
-    end
-  end
-
-  def render_final_score
-    render :score, UI::Label do |label|
-      label.text_alignment = :center
-    end
-  end
-
-  def render_target_color_square
-    render :target_color_wrapper, UI::View do |header|
-      header.margin = 5
-      render :none, UI::View do |border|
-        border.background_color = :silver
-        border.width = 72
-        border.height = 72
-        border.align_self = :center
-        render :target_color, UI::View do |square|
-          square.width = 70
-          square.height = 70
-          square.margin = 1
-          square.align_self = :center
-        end
-      end
-    end
-  end
-
-  def render_player_color_square
-    render :none, UI::View do |header|
-      header.margin = 5
-      render :none, UI::View do |border|
-        border.background_color = :silver
-        border.width = 72
-        border.height = 72
-        border.align_self = :center
-        render :player_color, UI::Label do |square|
-          square.width = 70
-          square.height = 70
-          square.margin = 1
-          square.align_self = :center
-          square.text_alignment = :center
-          square.font = font.merge(size: 30)
-        end
-      end
-    end
-  end
-
-  def header_width
-    @header_width ||= width_for(:target_color_wrapper)
-  end
-
-  def cell_width
-    @cell_width ||= (header_width - 30).fdiv(3)
-  end
-
-  def render_rgb_grid
-    header_width = width_for(:target_color_wrapper)
-
-    cell_width = (header_width - 30).fdiv(3)
-
-    render :none, UI::View do |grid|
-      grid.width = header_width
-      grid.margin = 5
-      grid.flex_direction = :row
-      grid.flex_wrap = :wrap
-      grid.justify_content = :center
-      grid.align_self = :center
-
-      render :none, UI::Label do |label|
-        label.width = cell_width
-        label.text = 'Red'
-        label.align_self = :center
-        label.text_alignment = :center
-      end
-
-      render :none, UI::Label do |label|
-        label.width = cell_width
-        label.text = 'Green'
-        label.align_self = :center
-        label.text_alignment = :center
-      end
-
-      render :none, UI::Label do |label|
-        label.width = cell_width
-        label.text = 'Blue'
-        label.align_self = :center
-        label.text_alignment = :center
-      end
-
-      cell_ids.each do |k, v|
-        render k, UI::Button do |button|
-          button.width = cell_width
-          button.margin = 1
-          button.padding = 1
-          button.title = v[:text]
-          button.on :tap do
-            game.send(v[:method], v[:value])
-            update
-          end
-          button.background_color = :white
-        end
-      end
-    end
-  end
-
-  def render_next_hole_option
-    render :none, UI::View do |view|
-      view.height = 40
-      render :next_hole_button, UI::Button do |button|
-        button.title = 'Next Hole'
-        button.color = bluish
-        button.font = font.merge(size: 20)
-        button.height = 40
-        button.background_color = :white
-        button.on :tap do
-          game.next_hole
-          set_random_stat_text
-          update
-        end
-      end
-
-      render :new_game_button, UI::Button do |button|
-        button.title = 'Go Again'
-        button.color = bluish
-        button.font = font.merge(size: 20)
-        button.height = 40
-        button.background_color = :white
-        button.on :tap do
-          save_history
-          new_game
-          set_random_stat_text
-          update
-          save_game
-        end
-      end
-    end
-  end
-
-  def render_stat_text
-    render :stat_text, UI::Label do |label|
-      label.text_alignment = :center
-      label.height = 80
-      label.margin = 10
-      label.font = font.merge(size: 16)
-    end
   end
 
   def font
--- config/ios.rb
+++ config/ios.rb
@@ -10,6 +10,8 @@ Motion::Project::App.setup do |app|
   # Use `rake ios:config' to see complete project settings.
   app.name = 'Color Golf'
   app.identifier = 'com.scratchworkdevelopment.colorgolf'
+  app.codesign_certificate = 'iPhone Developer: Amirali Rajan'
+  app.provisioning_profile = './profiles/Development_Wildcard.mobileprovision'
 
   # app.development do
   #   app.codesign_certificate = MotionProvisioning.certificate(
